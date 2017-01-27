8521fea7439349038c2e48aa5df2a44435d8db4e
alexandre <2a.alexandre@gmail.com>
2017-01-27 11:23:50 +0100

    UPDATE: re format JSON for tree selection

--- src/index.php
+++ src/index.php
@@ -8,31 +8,52 @@ $db = new DoctrineDatabase();
 
 $db->writeDatabaseYamlConfig();
 
+$arrayQuery = [
+    'from' => [
+        'registrant' => [
+            '0'            => 'id',
+            '1'            => 'first_name',
+            '2'            => 'last_name',
+            'civility'     => [
+                '0' => 'name',
+            ],
+            'registration' => [
+                '0'    => 'registration_amount',
+                '1'    => 'registration_date',
+                'user' => [
+                    '0' => 'first_name',
+                    '1' => 'last_name'
+                ]
+            ]
+        ]
+    ]
+];
+
+echo '<pre>';
+var_dump($arrayQuery);
+
+var_dump(json_encode($arrayQuery));
+
 $jsonQuery = '
 {
-    "from": {
-        "registrant": [
-            "id", "first_name", "last_name"
-        ],
-        "civility": [
-            "id", "name"
-        ],
-        "registration": [
-            "id", "registration_amount"
-        ]
-    },
-    "where": {
-        "AND": {
-            "civility.name": {
-                "EQUAL": ["Monsieur"]            
-            }
-        }
-    },
-    "orderBy": {
-        "asc": {
-            "post": ["name", "id"]
-        }
-    }
+   "from":{
+      "registrant":{
+         "0":"id",
+         "1":"first_name",
+         "2":"last_name",
+         "civility":[
+            "name"
+         ],
+         "registration":{
+            "0":"registration_amount",
+            "1":"registration_date",
+            "user":[
+               "first_name",
+               "last_name"
+            ]
+         }
+      }
+   }
 }
 ';
 
